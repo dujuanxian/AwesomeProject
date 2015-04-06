0b2bdb2a764c359bfe7ed0d4c3edbfc0e6e77000
Juan Du <juan.du@rea-group.com>
2015-04-06 22:22:53 +0800

    isolate the data

--- index.ios.js
+++ index.ios.js
@@ -11,17 +11,24 @@ var styles = require('./stylesheet');
 var MOCKED_MOVIES_DATA = require('./movies');
 
 var AwesomeProject = React.createClass({
+    getInitialState: function() {
+        return {
+            movies: null
+        }
+    },
     render: function() {
-        var movie = MOCKED_MOVIES_DATA[0];
         return (
             <View style={styles.container}>
-                <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
+                <Image source={{uri: this.state.movies.posters.thumbnail}} style={styles.thumbnail}/>
                 <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{movie.title}</Text>
-                    <Text style={styles.year}>{movie.year}</Text>
+                    <Text style={styles.title}>{this.state.movies.title}</Text>
+                    <Text style={styles.year}>{this.state.movies.year}</Text>
                 </View>
             </View>
         );
+    },
+    componentWillMount: function() {
+        this.setState({movies: MOCKED_MOVIES_DATA[0]});
     }
 });
 
