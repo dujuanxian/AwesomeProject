75b5b5582370fabc87ffc5d7802fb97b9bea76ae
Juan Du <juan.du@rea-group.com>
2015-04-06 18:49:11 +0800

    refactor stylesheet to a separate js

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
-  StyleSheet,
   Text,
   View,
 } = React;
+var styles = require('./stylesheet');
 
 var AwesomeProject = React.createClass({
   render: function() {
@@ -26,24 +22,6 @@ var AwesomeProject = React.createClass({
       </View>
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-  },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
